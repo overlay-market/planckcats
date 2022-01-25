e030a3fa6e596b5f6a6ce7276632043a4e91835d
bob431136 <96485257+bob431136@users.noreply.github.com>
2022-01-25 11:02:00 -0500

    add mint period

--- tests/minter/test_conftest.py
+++ tests/minter/test_conftest.py
@@ -1,5 +1,13 @@
+from brownie import chain
+
+
 def test_minter_fixture(minter, cat, gov):
+    # check constructor params set
     assert minter.pcd() == cat
+    assert minter.periodMint() == 86400
+    assert minter.capMint() == 3
+    assert minter.allowedMint() == 3
+    assert minter.timestampPeriodLast() == chain[-1]["timestamp"]
 
     # should have minter role
     assert cat.hasRole(cat.MINTER_ROLE(), minter) is True
